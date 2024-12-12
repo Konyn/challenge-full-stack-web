b0affb4bb629c14d7516819e9bfe2440f34b8ef1
Konyn <devcoelho00@gmail.com>
2024-12-11 23:06:01 -0300

    Added color pattern and set the theme to light

--- Frontend/src/plugins/vuetify.ts
+++ Frontend/src/plugins/vuetify.ts
@@ -1,19 +1,19 @@
-/**
- * plugins/vuetify.ts
- *
- * Framework documentation: https://vuetifyjs.com`
- */
+import "@mdi/font/css/materialdesignicons.css";
+import "vuetify/styles";
 
-// Styles
-import '@mdi/font/css/materialdesignicons.css'
-import 'vuetify/styles'
-
-// Composables
-import { createVuetify } from 'vuetify'
-
-// https://vuetifyjs.com/en/introduction/why-vuetify/#feature-guides
+import { createVuetify } from "vuetify";
 export default createVuetify({
   theme: {
-    defaultTheme: 'dark',
+    defaultTheme: "light",
+    themes: {
+      light: {
+        dark: false,
+        colors: {
+          primary: "#D51E30",
+          secondary: "#00B0B8",
+          gray: "#C5C2C0",
+        },
+      },
+    },
   },
-})
+});
